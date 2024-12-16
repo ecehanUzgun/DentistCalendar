0215c81c8c79e2db85ee355f9b0bea5878798784
ecehanUzgun <ecehan766@gmail.com>
2024-12-16 13:04:33 +0300

    Identity Configuration

--- DentistCalendar/Controllers/AccountController.cs
+++ DentistCalendar/Controllers/AccountController.cs
@@ -16,7 +16,85 @@ namespace DentistCalendar.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
             _roleManager = roleManager;
+        }
 
+        public async Task<IActionResult> Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                if (user != null) 
+                { 
+                    return View(user);
+                }
+            }
+            return View();
+        }
+
+        // Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                //Kullanıcı oluştur
+                AppUser appUser = new AppUser()
+                {
+                    UserName = user.Username,
+                    Email = user.Email
+                };
+
+                //Veritabanına kaydet
+                var result = await _userManager.CreateAsync(appUser, user.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    return View(user);
+                }
+            }
+            else
+            {
+                return View(user);
+            }
+        }
+
+        //Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel lvm, bool rememberMe)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = await _userManager.FindByEmailAsync(lvm.Email);
+                if (appUser != null) 
+                {
+                    var result = await _signInManager.PasswordSignInAsync(appUser, lvm.Password, rememberMe, false);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            return View(lvm);
+        }
+
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index","Home");
         }
     }
 }
